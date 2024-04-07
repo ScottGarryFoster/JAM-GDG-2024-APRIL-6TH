b41381e61adc9a3d60d9d7156585b48280645d11
Scott Foster <scottgarryfoster@googlemail.com>
2024-04-07 20:36:53 +0100

    Credits and Minor UI

---
+++ JAM-Game/Assets/Scripts/CreditsScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class CreditsScript : MonoBehaviour
+    {
+        public GameObject MainObj;
+        public GameObject CreditsObj;
+
+        public void ShowCredits()
+        {
+            MainObj.SetActive(false);
+            CreditsObj.SetActive(true);
+        }
+        
+        public void HideCredits()
+        {
+            MainObj.SetActive(true);
+            CreditsObj.SetActive(false);
+        }
+    }
+}
